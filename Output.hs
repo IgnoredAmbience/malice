e34261876da5755a2ce76460dc8bd69e742bb80a
Ethel Bardsley <ethel.bardsley@gmail.com>
2010-11-08 15:57:52 +0000

    Cleaned up some of the Output functions and added the program-return instructions

--- Output.hs
+++ Output.hs
@@ -2,16 +2,16 @@ module Output where
 import Types
 
 outputASM :: [SInst] -> [String]
-outputASM = concatMap toASM
+outputASM = concatMap toASM ++ ["pop ebx"] ++ ["mov eax,1"] ++ ["int 0x80"]
 
 toASM :: SInst -> [String]
 
-toASM SNot = "not [esp]" : []
-toASM SOr  = "pop eax" : "or [esp],eax" : []
-toASM SXor = "pop eax" : "xor [esp],eax" : []
-toASM SAnd = "pop eax" : "and [esp],eax" : []
-toASM SAdd = "pop eax" : "add [esp],eax" : []
-toASM SSub = "pop eax" : 'sub [esp],eax" : []
+toASM SNot = ["not [esp]"]
+toASM SOr  = ["pop eax"] ++ ["or [esp],eax"]
+toASM SXor = ["pop eax"] ++ ["xor [esp],eax"]
+toASM SAnd = ["pop eax"] ++ ["and [esp],eax"]
+toASM SAdd = ["pop eax"] ++ ["add [esp],eax"]
+toASM SSub = ["pop eax"] ++ ["sub [esp],eax"]
 --toASM SMul = 
 --toASM SDiv = 
 --toASM SMod = 
