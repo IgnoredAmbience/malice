a97c62c81a24f8dbd9666791a6b78beab6ddae2f
Joseph Slade <js5409@ic.ac.uk>
2010-11-09 00:57:05 +0000

    Main.hs now looks for -p to parse only, -l to parse and lex, -t to translate parse and lex. There is also -f $FILENAME to feed files in by name (easier for testing across a whole folder of files). One of these must be present. Also, not added particularly elegantly - there is a link in comments for a better way of doing it.

--- main.hs
+++ main.hs
@@ -4,4 +4,23 @@ import Parser
 import Semantics
 import Translator
 import Text.Groom
-main = getContents >>= putStrLn . groom . translate . parse . alexScanTokens
+import System.Environment
+
+
+-- TODO, tidy up this mo'fo. check http://leiffrenzel.de/papers/commandline-options-in-haskell.html for info about better command line arg stuff
+
+main :: IO()
+main = do
+  args <- getArgs
+  case args of
+    "-l":_      -> getContents   >>= putStrLn.groom.alexScanTokens
+    "-p":_      -> getContents   >>= putStrLn.groom.parse.alexScanTokens 
+    "-t":_      -> getContents   >>= putStrLn.groom.translate.parse.alexScanTokens
+    "-f":name:_ -> readFile name >>= putStrLn.groom.translate.parse.alexScanTokens
+    "-f":_      -> error "File name missing"  
+    _           -> error "Please pipe the contents of an alice file in"
+  
+  
+
+
+              
