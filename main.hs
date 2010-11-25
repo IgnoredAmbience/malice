7669d8681a6018f955ff48dc8ddeb2d1699165bd
MrSlade <js5409@ic.ac.uk>
2010-11-25 06:03:47 +0000

    Half way thru updating for command line args - don't pull off this yet.

--- main.hs
+++ main.hs
@@ -4,24 +4,39 @@ import Parser
 import Semantics
 import Translator
 import Output
---import Text.Groom
-import System.Environment
-
--- TODO, tidy up this mo'fo. check http://leiffrenzel.de/papers/commandline-options-in-haskell.html for info about better command line arg stuff
+import System (getArgs)
+import System.Console.GetOpt -- http://leiffrenzel.de/papers/commandline-options-in-haskell.html 
 
 main :: IO()
 main = do
   args <- getArgs
+  let (_,_,_) = getOpt RequireOrder options args
   case args of
---    "-l":_      -> getContents   >>= putStrLn . groom . alexScanTokens
---    "-p":_      -> getContents   >>= putStrLn . groom . parse . alexScanTokens 
---    "-a":_      -> getContents   >>= putStrLn . unlines . map groom . translate . parse . alexScanTokens
     "-f":name:_ -> readFile name >>= compile
     "-f":_      -> error "File name missing"  
     _           -> getContents   >>= compile
+
+data Flag = Semantics | Lexer | Assembler | Parser
+
+options :: [OptDescr Flag]
+options = [ Option ['S'] ["semantics"] (NoArg Semantics) "output the symbol  table",
+            Option ['L'] ["lexer"]     (NoArg Lexer)     "output the token list",
+            Option ['P'] ["parser"]    (NoArg Parser)    "output the AST",
+            Option ['A'] ["assembly"]  (NoArg Assembler) "output the assembler code"                        
+          ]
+
+
+
+
+
+
+
+
 
 compile :: String -> IO ()
 compile source = putStrLn . unlines . output st $ translate program
   where
     (st, _) = semantics program
     program = parse $ alexScanTokens source
+
+
