682f6a37d644fc012d800910f110bb9ceafbd3a9
Mr Slade <js5409@ic.ac.uk>
2010-12-16 03:19:57 +0000

    Added the ability to use some switches for output. Input must be redirected from stdin atm.

--- main.hs
+++ main.hs
@@ -7,23 +7,22 @@ import Output
 import System (getArgs)
 import Data.Maybe (fromMaybe)
 import System.Console.GetOpt -- http://leiffrenzel.de/papers/commandline-options-in-haskell.html 
-import Control.Monad (liftM)
-import Control.Applicative ((<$>),(<*>))
+import Text.Groom
 
 main :: IO()
 main = do
   args <- getArgs
-  {- let (flags,unknownFlags,errorMessages) = getOpt RequireOrder options args
+--  let (flags,unknownFlags,errorMessages) = getOpt RequireOrder options args
   case getOpt RequireOrder options args of
     (f, [] , [] )     -> processFlags f
     (_, unknowns, []) -> error $ "Did not recognise " ++ unwords unknowns
     (_,_,errorMsgs)      -> error $ concat errorMsgs
-   -}
-  case args of
+  
+{-  case args of
     "-f":name:_ -> readFile name >>= compile
     "-f":_      -> error "File name missing"  
     _           -> getContents   >>= compile
-    
+  -}  
 
 data Flag = Output String | Input String | OutputMade OutputStage
 data OutputStage = Lexer | Parser | Semantics | Assembly | Compile
@@ -33,7 +32,7 @@ options :: [OptDescr Flag]
 options = [ Option ['S','s'] ["semantics"] (NoArg (OutputMade Semantics)) "output the symbol table",
             Option ['L','l'] ["lexer"]     (NoArg (OutputMade Lexer)) "output the token list",
             Option ['P','p'] ["parser"]    (NoArg (OutputMade Parser)) "output the AST",
-            Option ['A','a'] ["assembly"]  (NoArg (OutputMade Assembly)) "output the generated assembly",                  
+            Option ['A','a'] ["assembly"]  (NoArg (OutputMade Compile)) "output the generated assembly",                  
             Option ['O','o'] ["out","output"]      (OptArg makeOut "FILE") "output to FILE",
             Option ['F','f'] ["in","input","file"] (OptArg input "FILE")   "input from FILE"
           ]
@@ -52,7 +51,12 @@ compile source =  putStrLn . unlines . concat . output symbolTables . translate 
 
 
 processFlags :: [Flag] -> IO () -- empty string is stdin / stdout respectively
-processFlags fs = undefined
+processFlags fs = input >>= 
+                  case compileStage of
+                      Lexer     -> putStrLn . groomString . show . alexScanTokens 
+                      Parser    -> putStrLn . groomString . show . parse . alexScanTokens
+                      Semantics -> putStrLn . groomString . show . snd . semantics . parse . alexScanTokens
+                      Compile   -> compile
   where
     (compileStage, inFrom, outTo) = foldr processFlagStep (Compile, "", "") fs
 
@@ -64,19 +68,9 @@ processFlags fs = undefined
             "" -> putStrLn
             xs -> writeFile xs
 
-    symTab  =  fst <$> semantics <$> program
-    program = parse . alexScanTokens <$> input
-
-    compileAction = case compileStage of
-                      Lexer     -> input >>= putStrLn . show . alexScanTokens 
-                      Parser    -> input >>= putStrLn . show . parse . alexScanTokens
-                      Semantics -> input >>= putStrLn . show . fst . semantics . parse . alexScanTokens
-                      -- Assembly  -> input >>= putStrLn . unlines . output symTab . translate . parse . alexScanTokens
-                      Compile   -> undefined
-
+    
     processFlagStep :: Flag -> (OutputStage, String, String) -> (OutputStage, String, String) 
     processFlagStep f (s, i, o) = case f of
                                     OutputMade s' -> (min s s', i, o)
                                     Input inFile   -> (s, inFile, o)
                                     Output outFile -> (s, i, outFile)
-
