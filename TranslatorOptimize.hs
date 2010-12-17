a87611159bd55bdea45ec813ec8f75be6d9b293c
Joseph Slade <js5409@glyph08.doc.ic.ac.uk>
2010-12-17 17:35:20 +0000

    dded compile time errors on div 0

--- TranslatorOptimize.hs
+++ TranslatorOptimize.hs
@@ -1,11 +1,11 @@
-module TranslatorOptimize(transOptimize) where
+module TranslatorOptimize where
 import Types
+
 import Data.Bits
 
 
-transOptimize :: [SymbolTbl] -> [[SInst]] -> [[SInst]]
-transOptimize symTab = {-zipWith removeVariables symTab.-} map constantFold
-
+transOptimize :: [[SInst]] -> [[SInst]]
+transOptimize = map constantFold
 
 constantFold :: [SInst] -> [SInst]
 constantFold input
@@ -17,24 +17,18 @@ constantFold' :: [SInst] -> [SInst]
 
 constantFold' (SPushI i : SNot : ss) = (SPushI (complement i) : constantFold' ss)
 constantFold' (SPushI i : SNeg : ss) = (SPushI (-i) : constantFold' ss)
-constantFold' (SPushI i1 : SPushI i2 : SOr : ss) = (SPushI (i1.|.i2) : constantFold' ss)
+constantFold' (SPushI i1 : SPushI i2 : SOr : ss) =  (SPushI (i1.|.i2) : constantFold' ss)
 constantFold' (SPushI i1 : SPushI i2 : SXor : ss) = (SPushI (xor i1 i2) : constantFold' ss)
 constantFold' (SPushI i1 : SPushI i2 : SAnd : ss) = (SPushI (i1.&.i2) : constantFold' ss)
 constantFold' (SPushI i1 : SPushI i2 : SAdd : ss) = (SPushI (i1+i2) : constantFold' ss)
 constantFold' (SPushI i1 : SPushI i2 : SSub : ss) = (SPushI (i1-i2) : constantFold' ss)
 constantFold' (SPushI i1 : SPushI i2 : SMul : ss) = (SPushI (i1*i2) : constantFold' ss)
+
+constantFold' (SPushI _ : SPushI 0 : SDiv : _) = error $ "Attempted to divide by 0"
+constantFold' (SPushI _ : SPushI 0 : SMod : _) = error $ "Attempted to divide by 0"
+
 constantFold' (SPushI i1 : SPushI i2 : SDiv : ss) = (SPushI (div i1 i2) : constantFold' ss)
 constantFold' (SPushI i1 : SPushI i2 : SMod : ss) = (SPushI (mod i1 i2) : constantFold' ss)
 
 constantFold' (s:ss) = (s:constantFold' ss)
 constantFold' [] = []
-
-removeVariables :: SymbolTbl -> [SInst] -> [SInst]
-removeVariables = undefined
-
-
-getVarCount :: SymbolTbl -> [SInst] -> [(String,Type,Integer)]
-getVarCount symTab instrs = undefined -- toList symTab
-    where
-      x = undefined
-
