83e9399457daf6f643e545abe1661cbc13b8aa10
Thomas Wood <grand.edgemaster@gmail.com>
2010-12-17 14:11:09 +0000

    Fix strlens

--- Output.hs
+++ Output.hs
@@ -23,8 +23,8 @@ symbolToDef name x = name ++ " TODO UNKNOWN " ++ show x
 symbolToDef name x = name ++ " TODO UNKNOWN " ++ show x
 
 outputDataTable :: DataTbl -> [String]
-outputDataTable dt = ["section .data"] ++ (map stringDef $ toList dt) ++ (map stringLenDef $ elems dt)
+outputDataTable dt = ["section .data"] ++ concat ( zipWith (++) (map stringDef $ toList dt) (map stringLenDef $ elems dt) )
 
-stringDef (value, hash) = hash ++ ":\tdb\t`" ++ value ++ "`"
-stringLenDef hash = "len" ++ hash ++ ":\tequ\t$-" ++ hash
+stringDef (value, hash) = [hash ++ ":\tdb\t`" ++ value ++ "`"]
+stringLenDef hash = ["len" ++ hash ++ ":\tequ\t$-" ++ hash]
 
