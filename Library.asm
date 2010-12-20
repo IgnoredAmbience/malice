e2b3b88c509de2fd6df5437ed43c43de3a4004b6
Thomas Wood <grand.edgemaster@gmail.com>
2010-12-20 12:07:31 +0000

    Remove warning on input_int

--- Library.asm
+++ Library.asm
@@ -34,9 +34,6 @@ _start:
   int 80H
 
 input_int:
-; README: THIS BREAKS WHEN READING IN FROM A PIPE
-; (however, this works fine from the terminal)
-  
   mov edi, [rbufptr]
   mov esi, [rbuflen]
   xor ecx,ecx
